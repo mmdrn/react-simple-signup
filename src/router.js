440c289943c6d9fdbbf8cada40405b8445e26544
mohammadrn <mr.nezamdust@gmail.com>
2022-10-06 18:39:05 +0330

    home route added

--- src/router.js
+++ src/router.js
@@ -2,8 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
 import NotFound from "./routes/NotFound";
+import Home from "./routes/Home";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
   {
     path: "/login",
     element: <Login />,
