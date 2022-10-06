af6a14261ede9d065f1207b02879f60ceec80b90
mohammadrn <mr.nezamdust@gmail.com>
2022-10-06 18:25:02 +0330

    notfound route added

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
+import NotFound from "./routes/NotFound";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <Signup />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
 
---
+++ src/routes/NotFound/index.jsx
@@ -0,0 +1,9 @@
+import "./style.scss";
+
+export default function NotFound() {
+  return (
+    <div className="main-wrapper notfound-page">
+      <p className="text">Page Not Found!!!</p>
+    </div>
+  );
+}
